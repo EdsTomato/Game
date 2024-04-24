4ed5cf42f62e3b99be53a2c1c7e7475e308d080b
EdsTomato <Batuhanherdem09@gmail.com>
2024-04-24 20:17:34 +0200

    refactoring of print methods, new printer class created

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,5 +1,6 @@
 package main.java.com.game;
 
+import main.java.com.game.ui.TextPrinter;
 import main.java.com.game.character.Character;
 import main.java.com.game.item.Item;
 import main.java.com.game.character.Inventory;
@@ -7,46 +8,19 @@ import main.java.com.game.character.Inventory;
 import java.util.Scanner;
 
 public class Main {
-    // ANSI escape code to start italicized text
-    private static final String ITALIC_ON = "\033[3m";
-    // ANSI escape code to end italicized text / reset formatting
-    private static final String ITALIC_OFF = "\033[0m";
     public static void main(String[] args) throws InterruptedException {
         Scanner scan = new Scanner(System.in); //scanner for user input
 
         /*----------------Initial Character Set Up------------------------*/
         System.out.print("Name: ");
         String playerName = scan.nextLine();
-        printLine();
+        TextPrinter.printLine();
         Character character = new Character(playerName, 35);
         /*---------------------------------------------------------------*/
 
-        System.out.println("Name: "+ character.getName() + " Health: " + character.getHealth() + "/100");
-        printLine();
-        printAnimatedText("Where am I?", 175, 0); // Using ANSI codes for italics
+        TextPrinter.printStats(character);
+        TextPrinter.printLine();
+        TextPrinter.printNarratorText("You wake up naked in an alley. Your head hurts and you can't remember anything",100);
+        TextPrinter.printCharacterText("Where am I?", 175); // Using ANSI codes for italics
     }
-
-    /*Helper function for UI in Terminal*/
-    public static void printLine(){
-        System.out.println("---------------------------------------");
-    }
-
-    /*Function to print text with animation*/
-    /*If option is 0 text is also italic*/
-    public static void printAnimatedText(String text, int delay, int option) throws InterruptedException {
-        if(option == 0){
-            for (char c : text.toCharArray()) { // Iterate over each character in the text
-                System.out.print(ITALIC_ON + c  + ITALIC_OFF); // Print the italic character without a newline
-                Thread.sleep(delay); // Pause for the specified delay
-            }
-        }else{
-            for (char c : text.toCharArray()) { // Iterate over each character in the text
-                System.out.print(c); // Print the character without a newline
-                Thread.sleep(delay); // Pause for the specified delay
-            }
-        }
-        System.out.println();// Move to the next line after printing the entire text
-    }
-
-
 }
---
+++ src/main/java/com/game/ui/TextPrinter.java
@@ -0,0 +1,48 @@
+package main.java.com.game.ui;
+
+import main.java.com.game.character.Character;
+
+//This class is used for UI text output
+public class TextPrinter {
+    // ANSI escape code to start italicized text
+    private static final String ITALIC = "\033[3m";
+    // ANSI escape code to start bold text
+    private static final String BOLD = "\033[1m";
+    // ANSI escape code to start green text
+    private static final String GREEN = "\033[32m";
+    // ANSI escape code to reset formatting (turn off bold, italics, etc.)
+    private static final String ANSI_OFF = "\033[0m";
+
+    public static void printCharacterText(String text, int delay) throws InterruptedException {
+        printWithStyle(text, delay, ITALIC);
+    }
+
+    public static void printNarratorText(String text, int delay) throws InterruptedException {
+        printWithStyle(text, delay, BOLD);
+    }
+
+    public static void printSystemText(String text, int delay) throws InterruptedException {
+        printWithStyle(text, delay, GREEN);
+    }
+
+    public static void printNormalText(String text, int delay) throws InterruptedException {
+        printWithStyle(text, delay, ""); // Normal for default style
+    }
+
+    public static void printStats(Character character){
+        System.out.println(GREEN + "Name: "+ character.getName());
+        System.out.println(GREEN + "Health: " + character.getHealth() + "/100" + ANSI_OFF );
+    }
+
+    public static void printLine(){
+        System.out.println("---------------------------------------");
+    }
+
+    public static  void printWithStyle(String text, int delay, String style) throws InterruptedException {
+        for (char c : text.toCharArray()) {
+            System.out.print(style + c + ANSI_OFF); // Apply style and reset
+            Thread.sleep(delay); // Pause for the specified delay
+        }
+        System.out.println(); // Move to the next line
+    }
+}
